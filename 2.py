0358b473963848416d0148053beafdf45e1dcdc3
Dmytro-Klepak <dmitrooleksandrovic33@gmail.com>
2024-12-23 00:52:33 +0200

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # Вводимо дані користувача
+... name = input("Введіть ваше ім'я: ")
+... surname = input("Введіть ваше прізвище: ")
+... age = int(input("Введіть ваш вік: "))
+... 
+... # Виводимо тип та значення кожної змінної
+... print("Тип змінної 'name':", type(name))
+... print("Значення змінної 'name':", name)
+... 
+... print("Тип змінної 'surname':", type(surname))
+... print("Значення змінної 'surname':", surname)
+... 
+... print("Тип змінної 'age':", type(age))
